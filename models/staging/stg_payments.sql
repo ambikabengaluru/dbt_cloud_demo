60dab504009a6de72d9d7e35ac21415913725210
Ambika S <dsambika947@gmail.com>
2026-08-17 04:59:20 +0000

    staging_folder_update_1

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,36 @@
+-- with source as (
+    
+--     {#-
+--     Normally we would select from the table here, but we are using seeds to load
+--     our data in this project
+--     #}
+--     select * from {{ ref('raw_payments') }}
+
+-- ),
+
+-- renamed as (
+
+--     select
+--         id as payment_id,
+--         order_id,
+--         payment_method,
+
+--         --`amount` is currently stored in cents, so we convert it to dollars
+--         amount / 100 as amount
+--         {# {{ cents_to_dollars('amount') }} as amount_usd #}
+
+--     from source
+
+-- )
+
+-- select * from renamed
+
+select
+   id as payment_id,
+   order_id as order_id,
+   payment_method as payment_method,
+   status,
+   -- amount is stored in cents, convert it to dollars
+   amount / 100 as amount
+
+from {{ ref('raw_payments') }}
