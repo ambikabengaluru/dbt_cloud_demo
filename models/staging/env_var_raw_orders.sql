8c737a4060b2b675b5698e340b1931a5fb536696
Ambika S <dsambika947@gmail.com>
2026-09-08 05:50:08 +0000

    updated env

--- models/staging/env_var_raw_orders.sql
+++ models/staging/env_var_raw_orders.sql
@@ -1,4 +1,3 @@
 select *
 from {{ source('jaffle_shop', 'raw_orders') }}
-
-where order_date >= "{{ env_var('START_DATE', '2024-01-01') }}"
+where order_date >= '{{ env_var('START_DATE', '2024-01-01') }}'
