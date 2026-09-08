8b62fbb06e99c5c9b03fec2a97887169b8512f9b
Ambika S <dsambika947@gmail.com>
2026-09-08 05:31:27 +0000

    updated

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -10,9 +10,7 @@
 
 {{ config(
     materialized='table',
-    tags=['sample', 'daily'],
-    pre_hook="CREATE TABLE IF NOT EXISTS audit_table (model_name VARCHAR, executed_at TIMESTAMP)",
-    post_hook="INSERT INTO audit_table (model_name, executed_at) VALUES ('my_first_dbt_model', CURRENT_TIMESTAMP())"
+    tags=['sample', 'daily']
 ) }}
 
 with source_data as (
